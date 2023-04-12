f8e96477b10a24757248466692fff881d12ef992
Leon <ajnj199320@gmail.com>
2023-04-12 15:51:15 +0300

    Configuration bean definitions, BeanFactoryPostProcessor, Ordered, PriorityOrdered

---
+++ src/main/java/org/example/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,27 @@
+package org.example.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.core.annotation.Order;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+
+//        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+//        for (String beanDefinitionName : beanDefinitionNames) {
+//            BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+//            List<ConstructorArgumentValues.ValueHolder> genericArgumentValues = beanDefinition.getConstructorArgumentValues().getGenericArgumentValues();
+//            genericArgumentValues.stream().map(ConstructorArgumentValues.ValueHolder::getName).forEach(System.out::println);
+//        }
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
---
+++ src/main/java/org/example/spring/bfpp/VerifyPropertyBeanFactoryPostProcessor.java
@@ -0,0 +1,24 @@
+package org.example.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+
+public class VerifyPropertyBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+//        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+//        for (String beanDefinitionName : beanDefinitionNames) {
+//            BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+//            List<ConstructorArgumentValues.ValueHolder> genericArgumentValues = beanDefinition.getConstructorArgumentValues().getGenericArgumentValues();
+//            genericArgumentValues.stream().map(ConstructorArgumentValues.ValueHolder::getName).forEach(System.out::println);
+//        }
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}

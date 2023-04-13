aa991f1d4851e8c234de92c3fd03f3a41ede25e2
Leon <ajnj199320@gmail.com>
2023-04-13 15:17:15 +0300

    AutowiredAnnotationBeanPostProcessor for @Resource, @Autowired, @Qualifier and @Value annotations

--- src/main/java/org/example/spring/database/repository/CompanyRepository.java
+++ src/main/java/org/example/spring/database/repository/CompanyRepository.java
@@ -1,19 +1,30 @@
 package org.example.spring.database.repository;
 
 import org.example.spring.bpp.annotation.Auditing;
-import org.example.spring.bpp.annotation.InjectBean;
 import org.example.spring.bpp.annotation.Transaction;
 import org.example.spring.database.entity.Company;
 import org.example.spring.database.jdbc.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CRUDRepository<Company, Integer> {
-    @InjectBean
-    private  ConnectionPool connectionPool;
+//    @InjectBean
+//    @Qualifier("connectionPool1")
+    @Autowired
+    private ConnectionPool connectionPool1;
+    @Autowired
+    private List<ConnectionPool> connectionPools;
+
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
     private void postConstruct() {

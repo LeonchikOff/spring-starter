bba66c2457c0cba04cf78f8b39584722243238b9
Leon <ajnj199320@gmail.com>
2023-04-14 16:17:24 +0300

    Scope, JSR 330 Dependency Injection for Java

--- src/main/java/org/example/spring/database/repository/CompanyRepository.java
+++ src/main/java/org/example/spring/database/repository/CompanyRepository.java
@@ -6,12 +6,15 @@ import org.example.spring.database.entity.Company;
 import org.example.spring.database.jdbc.ConnectionPool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 @Repository
 @Transaction
 @Auditing
@@ -44,4 +47,17 @@ public class CompanyRepository implements CRUDRepository<Company, Integer> {
     public void delete(Company entity) {
         System.out.println("delete method...");
     }
+
+    public ConnectionPool getConnectionPool() {
+        return connectionPool;
+    }
+
+    public List<ConnectionPool> getConnectionPools() {
+        return connectionPools;
+    }
+
+    public Integer getPoolSize() {
+        return poolSize;
+    }
 }
+
--- src/main/java/org/example/spring/database/repository/UserRepository.java
+++ src/main/java/org/example/spring/database/repository/UserRepository.java
@@ -2,8 +2,11 @@ package org.example.spring.database.repository;
 
 import org.example.spring.database.jdbc.ConnectionPool;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Repository
 public class UserRepository {
     private final ConnectionPool connectionPool;
@@ -11,5 +14,9 @@ public class UserRepository {
     @Autowired
     public UserRepository(ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
+    }
+
+    public ConnectionPool getConnectionPool() {
+        return connectionPool;
     }
 }
